d9d6dee53697f9ad7a7aa48c7d52f4fe8e230f03
Ents <entsupml@gmail.com>
2023-08-25 18:52:33 +0300

    Added HAVING() > minCount

--- src/UPDM/Factory.py
+++ src/UPDM/Factory.py
@@ -6,9 +6,9 @@ from src.UPDM.Schema.Omop import Omop
 
 def create_schema_by_config(config) -> BaseSchema:
     if config['schema'] == 'Omop2':
-        return Omop2(config['dsn'], config['minCount'])
+        return Omop2(config['dsn'], config['min_count'])
     if config['schema'] == 'Omop':
-        return Omop(config['dsn'], config['minCount'])
+        return Omop(config['dsn'], config['min_count'])
     if config['schema'] == 'disabled':
         return Disabled()
 
