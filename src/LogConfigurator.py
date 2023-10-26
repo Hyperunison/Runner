b8348c01dc17ecaed78fa342419e0cf84c81f500
yu.marochkina <yu.marochkina@b2b-center.ru>
2023-10-26 23:54:48 +0400

    DEV-550 change log format

--- src/LogConfigurator.py
+++ src/LogConfigurator.py
@@ -1,16 +1,31 @@
 import logging
-import sys
+from pythonjsonlogger import jsonlogger
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        log_record['log.level'] = record.levelname
+        del log_record['level']
+        log_record['@timestamp'] = record.asctime
+        del log_record['asctime']
 
 
 def configure_logs(config, message: str = ''):
     level = int(config['log']['level'])
-    format = "{'time':'%(asctime)s', 'name': '%(name)s', 'level': '%(levelname)s', 'message': '%(message)s'}"
-    file_formatter=logging.Formatter(format)
-    logging.basicConfig(level=level, format=format)
-    logging.getLogger().setLevel(level)
+    format_name = config['log']['format'] if 'format' in config['log'] else 'raw'
+    if format_name == 'json':
+        logger = logging.getLogger()
+        logger.addHandler(logging.StreamHandler())
+        log_formatter = CustomJsonFormatter('%(@timestamp)s %(asctime)s %(log.level)s %(level)s %(name)s %(message)s')
+    else:
+        format ='%(asctime)s [%(levelname)s] [{}] %(message)s'.format(message)
+        log_formatter=logging.Formatter(format)
+        logging.basicConfig(level=level, format=format)
+        logging.getLogger().setLevel(level)
 
     for handler in logging.getLogger().handlers:
-        handler.setFormatter(file_formatter)
+        handler.setFormatter(log_formatter)
 
     logging.basicConfig()
     logging.getLogger().setLevel(level)
