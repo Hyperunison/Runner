8218cd7e20dc1fb092927f11177edf92e3e5c536
Ents <entsupml@gmail.com>
2024-02-23 16:12:49 +0300

    Fix typo

--- src/Service/Workflows/OMOPification/OMOPofication.py
+++ src/Service/Workflows/OMOPification/OMOPofication.py
@@ -80,7 +80,7 @@ class OMOPofication(WorkflowBase):
                   "condition_start_datetime", "condition_end_date", "condition_end_datetime", "condition_type_concept_id",
                   "condition_status_concept_id", "stop_reason", "provider_id", "visit_occurrence_id", "visit_detail_id",
                   "condition_source_value", "condition_source_concept_id", "condition_status_source_value"]
-        filename = self.dir + "/person.csv"
+        filename = self.dir + "/condition_occurrence.csv"
         with open(filename, 'w', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=header)
             writer.writeheader()  # Writes the keys as headers
