42d1f775b7be0a9eab8815062289e7a759778758
Ents <entsupml@gmail.com>
2024-06-18 16:51:37 +0300

    fix export person to OMOP

--- src/Service/Workflows/OMOPification/OMOPoficationBase.py
+++ src/Service/Workflows/OMOPification/OMOPoficationBase.py
@@ -7,22 +7,20 @@ from src.Service.UCDMResolver import UCDMConvertedField
 class OMOPoficationBase:
     dir: str = "var/"
 
-    def transform_person_id_to_integer(self, origin_value):
-        return int(float(self.clear_person_id(origin_value)))
-
     def get_dir(self):
         return self.dir
 
-    def clear_person_id(self, origin_value):
-        return origin_value.replace("DKFZ-I00", "")
-
-    def render_omop_id(self, row: Dict[str, UCDMConvertedField],  field: str) -> str:
+    def render_omop_id(self, row: Dict[str, UCDMConvertedField], field: str) -> str:
         return row[field].omop_id if field in row and row[field] is not None else ''
+
     def render_ucdm_value(self, row: Dict[str, UCDMConvertedField], field: str) -> str:
         return row[field].ucdm_value if field in row and row[field] is not None else ''
+
     def render_biobank_value(self, row: Dict[str, UCDMConvertedField], field: str) -> str:
         return row[field].biobank_value if field in row and row[field] is not None else ''
+
     def render_date(self, row: Dict[str, UCDMConvertedField], field: str) -> str:
         return row[field].biobank_value if (field in row and row[field] is not None) else ''
+
     def render_datetime(self, row: Dict[str, UCDMConvertedField], field: str) -> str:
         return row[field].biobank_value if (field in row and row[field] is not None) else ''
