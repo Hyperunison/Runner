dbe6add8a7f724d791256f3263bc8295ae298820
Ents <entsupml@gmail.com>
2024-07-31 20:00:21 +0300

    Added logging of reasons of skipped rows

--- src/Service/Workflows/OMOPification/OMOPofication.py
+++ src/Service/Workflows/OMOPification/OMOPofication.py
@@ -56,7 +56,7 @@ class OMOPofication(WorkflowBase):
 
                 if len(ucdm) > 0:
                     filename = self.dir + "{}.csv".format(table_name)
-                    self.build(filename, ucdm, fields_map)
+                    self.build(filename, ucdm, fields_map, message.id, api_logger)
                     api_logger.write(message.id, "{}.csv file was written".format(table_name))
                     if self.may_upload_private_data:
                         s3_path = s3_folder + table_name + '.csv'
@@ -78,20 +78,34 @@ class OMOPofication(WorkflowBase):
         percent = int(round(step / length * 100, 0))
         self.api.set_job_state(run_id=str(id), state=state, percent=percent, path=path)
 
-    def build(self, filename: str, ucdm: List[Dict[str, UCDMConvertedField]], fields_map: Dict[str, Dict[str, str]]):
+    def build(
+            self,
+            filename: str,
+            ucdm: List[Dict[str, UCDMConvertedField]],
+            fields_map: Dict[str, Dict[str, str]],
+            runner_message_id: int,
+            api_logger: ApiLogger
+    ):
         with open(filename, 'w', newline='') as file:
             header = [item['name'] for item in fields_map.values()]
             writer = csv.DictWriter(file, fieldnames=header)
             writer.writeheader()
+            skip_rows: List[str] = []
             for row in ucdm:
                 output = {}
-                skip = False
+                skip_reasons: List[str] = []
                 for key, val in row.items():
                     value = val.export_value
                     if (value == '' or value is None or value == '0' or value == 0) and fields_map[key]['isRequired']:
-                        skip = True
+                        skip_reasons.append("{} is empty, but required (value={})".format(key, value))
                     field_name = fields_map[key]['name']
                     output[field_name] = value if value is not None else ''
 
-                if not skip:
+                if len(skip_reasons) == 0:
                     writer.writerow(output)
+                else:
+                    row_str: Dict[str, str] = {}
+                    for k, v in row.items():
+                        row_str[k] = v.export_value
+                    skip_rows.append("Skip row as [{}]. Row={}".format(", ".join(skip_reasons), str(row_str)))
+            api_logger.write(runner_message_id, '\n'.join(skip_rows))
