a82c8052f010d670cc5378019005993921dadb26
Ents <entsupml@gmail.com>
2024-07-31 13:40:05 +0300

    More logging

--- src/Service/Workflows/StrToIntGenerator.py
+++ src/Service/Workflows/StrToIntGenerator.py
@@ -1,4 +1,5 @@
 from typing import Dict
+import logging
 
 
 class StrToIntGenerator:
@@ -8,9 +9,12 @@ class StrToIntGenerator:
     def get_int(self, string: any) -> int:
         string: str = str(string)
         if string in self.map:
+            logging.debug("Resolve {} for str {} from memory".format(self.map[string], string))
             return self.map[string]
 
         self.counter += 1
         self.map[string] = self.counter
+
+        logging.debug("Resolve new value {} for str {}. Dict len={}".format(self.counter, string, len(self.map.values())))
 
         return self.counter
