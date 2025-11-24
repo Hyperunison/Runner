5a72e5001eb1ebbdd5e3669cea4b8de07fc7d960
Ents <entsupml@gmail.com>
2025-11-24 12:04:30 +0300

    Enlarge unison-runner-dqd-sdtmig-3.4 image

--- Resources/dqd-sdtmig-3.4/build.sh
+++ Resources/dqd-sdtmig-3.4/build.sh
@@ -1,2 +1,2 @@
-docker build -t entsupml/unison-runner-dqd-sdtmig-3.4:0.13 .
-docker push entsupml/unison-runner-dqd-sdtmig-3.4:0.13
+docker build -t entsupml/unison-runner-dqd-sdtmig-3.4:0.14 .
+docker push entsupml/unison-runner-dqd-sdtmig-3.4:0.14
