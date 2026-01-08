3032e9c5df3a5dc81499ea7ce53613759f71c814
Ents <entsupml@gmail.com>
2026-01-08 10:39:50 +0300

    Build scripts added

--- Resources/dqd-omop-5.4/build.sh
+++ Resources/dqd-omop-5.4/build.sh
@@ -1,2 +1,5 @@
-docker build -t entsupml/unison-runner-dqd-omop-5.4 .
-docker push entsupml/unison-runner-dqd-omop-5.4
+#!/bin/bash -e
+version=$1
+
+docker build -t entsupml/unison-runner-dqd-omop-5.4:$version .
+docker push entsupml/unison-runner-dqd-omop-5.4:$version
